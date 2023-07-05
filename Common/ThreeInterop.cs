f15876313321fefdf63ecbf79f637034d2ee3617
Luis Llamas <luisllamas@hotmail.com>
2023-07-05 17:16:01 +0200

    Add project files.

---
+++ Common/ThreeInterop.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace WpfBlazor.Common
+{
+    public class ThreeInterop
+    {
+        public async Task Init(IJSRuntime js)
+        {            
+            var objectRef = DotNetObjectReference.Create(this);
+            await js.InvokeVoidAsync("init", objectRef);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Extensions.DependencyInjection;
+using WpfBlazor.Common;
+
+namespace WpfBlazor;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddWpfBlazorWebView();
+        serviceCollection.AddSingleton<ThreeInterop>();
+        Resources.Add("services", serviceCollection.BuildServiceProvider());
+    }
+}
